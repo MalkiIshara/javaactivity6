7aa6ec033c8d51a34ce8891c42be072dd9c6a9d6
MalkiIshara <85068508+MalkiIshara@users.noreply.github.com>
2021-06-08 10:47:09 +0530

    Add files via upload

---
+++ AreaOfTriangle3.java
@@ -0,0 +1,9 @@
+class AreaOfTriangle3{
+	public static void main(String[] args){
+		//x-triangle of base
+		//y-triangle of height
+		int x=12,y=6,Area;
+		Area=(x*y)/2;
+			System.out.println(Area);
+	}
+}
