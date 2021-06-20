7f9c2ac1c0a0faadf8d1f43e08d9f8eeafc8b449
Malki <malkiishara1998@gmail.com>
2021-06-20 22:18:33 +0530

    first commit

---
+++ GoToSchoolOrNot5.java
@@ -0,0 +1,11 @@
+class GoToSchoolOrNot5{
+	public static void main(String[] args){
+		int whether_on_day=x;
+		x=1;
+		if(whether_on_day =rains){
+			System.out.println("I do not go to school");
+		}else{
+			System.out.println("I go to school");
+		}
+	}
+}
